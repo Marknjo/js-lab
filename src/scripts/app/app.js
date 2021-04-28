41a579a2d6542675a553fba0373802643107257e
Marknjo <marknjo7@gmail.com>
2021-04-28 13:17:36 +0300

    Declaring variables: Let, const, & Var

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -11,23 +11,43 @@ let lastName = "Kafna";
 
 console.log(firstName + " " + lastName); */
 
-let javaScriptIsFun = true;
-console.log(javaScriptIsFun);
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
 
-// console.log(typeof true);
-console.log(typeof javaScriptIsFun);
-// console.log(typeof 23);
-// console.log(typeof "Jonas");
+// // console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof "Jonas");
 
-javaScriptIsFun = "YES!";
+// javaScriptIsFun = "YES!";
 
-console.log(typeof javaScriptIsFun);
+// console.log(typeof javaScriptIsFun);
 
-let year;
-console.log(year);
-console.log(typeof year);
+// let year;
+// console.log(year);
+// console.log(typeof year);
 
-year = 1991;
-console.log(typeof year);
+// year = 1991;
+// console.log(typeof year);
 
-console.log(typeof null);
+// console.log(typeof null);
+
+/**
+ *   Declaring Variables: Let, const
+ **/
+
+//let - declaring variables that can dynamically change their values later
+let age = 30;
+age = 31; //rassigning a value/mutate the variable
+
+//const - declare variable shich cannot be mutated/immutable
+//      - Const needs an initial variable
+const birthYear = 1991;
+// birthYear = 1990;
+
+//const job;
+
+//var - for regacy reasons - should be avoided. ES5
+
+var job = "Programmer";
+job = "teacher";
