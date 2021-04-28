a6c1c8df93a589c40873bc8f7113da25d0633c33
Marknjo <marknjo7@gmail.com>
2021-04-28 13:03:20 +0300

    Data types: Strings, Numbers, Undefined & Null

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -1,6 +1,6 @@
 import "./utils/utils";
 
-let js = "Amazing";
+/* let js = "Amazing";
 
 if (js === "Amazing") {
   //alert("JavaScript is amazing");
@@ -9,4 +9,25 @@ if (js === "Amazing") {
 let firstName = "Matilda";
 let lastName = "Kafna";
 
-console.log(firstName + " " + lastName);
+console.log(firstName + " " + lastName); */
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javaScriptIsFun = "YES!";
+
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
