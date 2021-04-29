7a8acbd6c286284416349acb8862e838523de00e
Marknjo <marknjo7@gmail.com>
2021-04-29 15:08:14 +0300

    Codding challenge #1 completion

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -114,3 +114,38 @@ const fruitProcessor = function (apples, oranges) {
 };
 
 console.log(fruitProcessor(2, 3));
+
+//////////////////////////////////////////////
+//                                          //
+//         Coding Challenge #1              //
+//                                          //
+//////////////////////////////////////////////
+
+//Scores Average calculator
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+
+const avgDolphins1 = calcAverage(44, 23, 71);
+const avgKoalas1 = calcAverage(65, 54, 49);
+
+const avgDolphins2 = calcAverage(85, 54, 41);
+const avgKoalas2 = calcAverage(23, 34, 27);
+
+console.log(avgDolphins1, avgKoalas1);
+console.log(avgDolphins2, avgKoalas2);
+
+const checkWinner = function (avgDolphins, avgKoalas) {
+  if (avgDolphins >= avgKoalas * 2) {
+    console.log(
+      `Dolphins 🏆 wins (${avgDolphins} points vs ${avgKoalas} points)`
+    );
+  } else if (avgKoalas >= avgDolphins * 2) {
+    console.log(
+      `Koalas 🏆 wins (${avgKoalas} points vs ${avgDolphins} points)`
+    );
+  } else {
+    console.log(`No team wins 😔`);
+  }
+};
+
+checkWinner(avgDolphins1, avgKoalas1);
+checkWinner(avgDolphins2, avgKoalas2);
