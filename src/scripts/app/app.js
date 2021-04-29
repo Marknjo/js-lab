921ebf002d8a8e01a0e4221390934c0023488209
Marknjo <marknjo7@gmail.com>
2021-04-29 13:40:08 +0300

    Learning js functions

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -1,7 +1,43 @@
 "use strict";
-import "./utils/utils";
+//import "./utils/utils";
 //////////////////////////////////////////////
 //                                          //
-//          Starter Sections Separtor       //
+//                Strict Mode               //
 //                                          //
 //////////////////////////////////////////////
+
+/* let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriversLicense = true;
+}
+
+if (hasDriversLicense) console.log("I can drive"); */
+
+//////////////////////////////////////////////
+//                                          //
+//          Functions Buddies               //
+//                                          //
+//////////////////////////////////////////////
+
+function logger() {
+  console.log("My name is Mark");
+}
+
+//callling, running, or invoking
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `🥤 Juice with ${apples} 🍎 apples and ${oranges} 🍊 oranges.`;
+
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
