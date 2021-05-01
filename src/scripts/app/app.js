3271073588c3ff8fe622584c16161fcd9d9ae98d
Marknjo <marknjo7@gmail.com>
2021-05-01 16:28:26 +0300

    Add basic game selectors

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -3,3 +3,12 @@
 //            PIG GAME PROJECT              //
 //                                          //
 //////////////////////////////////////////////
+
+//Selecting Elements
+const score0EL = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+score0EL.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
