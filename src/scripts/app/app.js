4433768be5d20d92d3cce6569b212cb545dff510
Marknjo <marknjo7@gmail.com>
2021-05-01 10:15:55 +0300

    Internal logic of the guess the No. app

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -16,10 +16,45 @@ document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
  */
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', evt => {
   const guess = Number(document.querySelector('.guess').value);
 
+  //zero or no number supplied logic
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+
+    //When the guess No. matches: Successful scenario
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    //Logic when the guess No. is higher than the secret no.
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      //reduce score
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    //Logic when the guess No. is below the secret no.
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      //reduce score
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
