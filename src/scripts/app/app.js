f7568b27938d253d15a40ec2c055eba8beb590fa
Marknjo <marknjo7@gmail.com>
2021-05-01 09:20:14 +0300

    Basic DOM manipulation

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -1,7 +1,16 @@
-import './utils/utils';
+//import './utils/utils';
 
 //////////////////////////////////////////////
 //                                          //
 //          Starter Sections Separtor       //
 //                                          //
 //////////////////////////////////////////////
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number';
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+
+console.log(document.querySelector('.guess').value);
