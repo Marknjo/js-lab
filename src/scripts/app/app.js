ae7188a6d9e25e200cac0dee59938556c93b1fd6
Marknjo <marknjo7@gmail.com>
2021-05-01 09:32:56 +0300

    Add event listener lesson

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -5,7 +5,7 @@
 //          Starter Sections Separtor       //
 //                                          //
 //////////////////////////////////////////////
-console.log(document.querySelector('.message').textContent);
+/* console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = '🎉 Correct Number';
 document.querySelector('.number').textContent = 13;
@@ -14,3 +14,12 @@ document.querySelector('.score').textContent = 10;
 document.querySelector('.guess').value = 23;
 
 console.log(document.querySelector('.guess').value);
+ */
+
+document.querySelector('.check').addEventListener('click', evt => {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  }
+});
