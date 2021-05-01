c831448baa6d16722c252946a2ff3dcd1bfa0e76
Marknjo <marknjo7@gmail.com>
2021-05-01 15:18:20 +0300

    Implementing close and open of modal using close button and overlay

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -1,7 +1,39 @@
-import "./utils/utils";
+//import "./utils/utils";
 
 //////////////////////////////////////////////
 //                                          //
 //          Starter Sections Separtor       //
 //                                          //
 //////////////////////////////////////////////
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+/**
+ * Open the modal
+ */
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+/**
+ * Close the modal
+ */
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+/**
+ * Loop through the buttons
+ */
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+//close modal even listeners-btn and overlay
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
