e1b7d761e16e748b2bb0cbba83d08a99926d5821
Marknjo <marknjo7@gmail.com>
2021-05-05 12:00:20 +0300

    Exploring default parameters in ES6 functions

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -1,5 +1,33 @@
 //////////////////////////////////////////////
 //                                          //
-//          Starter Sections Separtor       //
+//              Default Parameters          //
 //                                          //
 //////////////////////////////////////////////
+const bookings = [];
+const createBooking = function (
+  flightNum,
+  numPassagers = 1,
+  price = 199 * numPassagers
+) {
+  //   numPassagers = numPassagers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassagers,
+    price,
+  };
+
+  console.log(booking);
+
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+
+createBooking('LH123', undefined, 10000);
+
+console.log(bookings);
