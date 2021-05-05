909240f19a898247edd0628d78b35e416c1266f4
Marknjo <marknjo7@gmail.com>
2021-05-05 12:19:31 +0300

    Evaluating, passing arguments by value vs reference

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -3,7 +3,7 @@
 //              Default Parameters          //
 //                                          //
 //////////////////////////////////////////////
-const bookings = [];
+/* const bookings = [];
 const createBooking = function (
   flightNum,
   numPassagers = 1,
@@ -30,4 +30,41 @@ createBooking('LH123', 5);
 
 createBooking('LH123', undefined, 10000);
 
-console.log(bookings);
+console.log(bookings); */
+
+//////////////////////////////////////////////
+//                                          //
+//        Passing Arguments Works:          //
+//           Value VS Reference             //
+//                                          //
+//////////////////////////////////////////////
+
+const flight = 'LH234';
+const mark = {
+  name: 'Mark Njoroge',
+  passport: 23235209501,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 23235209501) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, mark);
+// console.log(flight);
+// console.log(mark);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(mark);
+checkIn(flight, mark);
+
+//Passing by Value or Reference: JS only passes by value
