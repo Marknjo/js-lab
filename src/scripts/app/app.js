67f8681fbb8301e3b461457706d681b53f26c28c
Marknjo <marknjo7@gmail.com>
2021-05-06 09:50:33 +0300

    Adding a separator with a title helper method for console logs

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -1,5 +1,22 @@
+/**
+ * Creates as a separator in the console
+ * @param {String} title
+ * @param {Number} separatorLen
+ */
+const consoleSeparator = (title = './END', separatorLen = 20) => {
+  console.log('\n\n');
+  console.log(
+    `${'-'.repeat(separatorLen)}     ${title}      ${'-'.repeat(separatorLen)}`
+  );
+  console.log('\n');
+};
+
 //////////////////////////////////////////////
 //                                          //
 //          Starter Sections Separtor       //
 //                                          //
 //////////////////////////////////////////////
+
+//Separator for console logs
+/////////////////////////////////////////////////
+consoleSeparator(undefined, 40);
