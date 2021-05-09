266829c39a0705bdc1c61f20d3fc8dda37c50110
Marknjo <marknjo7@gmail.com>
2021-05-09 13:51:39 +0300

    Number & Dates: Understanding Math methods & Rounding

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -64,7 +64,7 @@ const accounts = [account1, account2];
 //                                          //
 //////////////////////////////////////////////
 //
-console.log(23 === 23.0);
+/* console.log(23 === 23.0);
 
 //Base 10 - 0 to 9 1/10 = 0.1 3/10 = 3.33333333333
 //Binry base 2 - 0.1
@@ -104,6 +104,58 @@ console.log(Number.isFinite(0.3));
 console.log(Number.isInteger(23)); //intenger
 console.log(Number.isInteger(23.04)); //float
 console.log(Number.isInteger(23 / 0)); //Infinity
+ */
+//////////////////////////////////////////////
+//                                          //
+//              Math & Rounding             //
+//                                          //
+//////////////////////////////////////////////
+//
+
+console.log(Math.sqrt(25));
+console.log(25 ** (1 / 2));
+console.log(8 ** (1 / 3)); //cubic root
+
+console.log(Math.max(5, 18, 23, 11, 2));
+console.log(Math.max(5, 18, '23', 11, 2)); //does type coersion
+console.log(Math.max(5, 18, '23px', 11, 2)); // does not parse
+
+console.log(Math.min(5, 18, 23, 11, 2));
+
+console.log(Math.PI * Number.parseFloat('10px') ** 2); //area of a 10px circle
+
+console.log(Math.trunc(Math.random() * 6) + 1);
+
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min) + 1) + min;
+
+// 0...1 -> 0 ...(max - min) -> min... (ma)
+
+console.log(randomInt(10, 20));
+
+//Rounding intengers
+console.log(Math.trunc(23.3)); //remove any decimal parts
+
+console.log(Math.round(23.3));
+console.log(Math.round(23.9));
+
+console.log(Math.ceil(23.3));
+console.log(Math.ceil(23.9));
+
+console.log(Math.floor(23.3));
+console.log(Math.floor(23.9));
+
+console.log(Math.trunc(-23.3));
+console.log(Math.floor(-23.3));
+
+//Floating point numbers
+//Rounding decimals
+//returns a string and not a number
+const str = '2.7';
+console.log((2.7).toFixed(0));
+console.log((2.7).toFixed(3));
+console.log((2.345).toFixed(2));
+console.log(+(2.345).toFixed(2));
 
 //Separator for console logs
 /////////////////////////////////////////////////
