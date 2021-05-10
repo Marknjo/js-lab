89ed80f6ed6bbe01e1cc20cc1842454eb48f0733
Marknjo <marknjo7@gmail.com>
2021-05-10 14:19:15 +0300

    Advanced DOM: Understanding Tranversing the DOM methods and properties

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -282,6 +282,44 @@ document.querySelector('.nav__link').addEventListener('click', function (e) {
   //e.stopPropagation();
 }); */
 
+//////////////////////////////////////////////
+//                                          //
+//                  LESSONS                 //
+//           Tranversing the DOM            //
+//                                          //
+//////////////////////////////////////////////
+//
+
+const h1 = document.querySelector('h1');
+
+//going downwards: Child
+console.log(h1.querySelectorAll('.highlight'));
+console.log(h1.childNodes);
+console.log(h1.children); //only direct children
+console.log(h1.firstElementChild);
+console.log(h1.lastElementChild);
+h1.firstElementChild.style.color = 'white';
+h1.lastElementChild.style.color = 'orangered';
+
+//Going upwards: parents
+console.log(h1.parentNode);
+console.log(h1.parentElement);
+
+h1.closest('.header').style.background = 'var(--gradient-secondary)';
+h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+//Going sideways: Siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children);
+
+[...h1.parentElement.children].forEach(el => {
+  if (el !== h1) el.style.transform = 'scale(0.5)';
+});
 //Separator for console logs
 /////////////////////////////////////////////////
 consoleSeparator(undefined, 40);
