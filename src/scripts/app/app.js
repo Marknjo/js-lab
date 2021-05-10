ad8c39a9b4aeef5b51cf5f0d6b2bd0aaf2d56e5a
Marknjo <marknjo7@gmail.com>
2021-05-10 09:44:49 +0300

    Bankist Web: Set starter project files

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -13,6 +13,56 @@ const consoleSeparator = (title = './END', separatorLen = 20) => {
 
 //////////////////////////////////////////////
 //                                          //
+//           BANKIST WEB HOME PAGE          //
+//                                          //
+//////////////////////////////////////////////
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => {
+  btn.addEventListener('click', openModal);
+});
+/* for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal); */
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
+/**
+ * Displays the current year at the footer
+ */
+const displayYear = () => {
+  document.querySelector(
+    '.footer__year'
+  ).textContent = new Date().getFullYear();
+};
+displayYear();
+
+//////////////////////////////////////////////
+//                                          //
 //          Starter Sections Separtor       //
 //                                          //
 //////////////////////////////////////////////
