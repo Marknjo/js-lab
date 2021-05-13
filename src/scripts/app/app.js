f157643382ec03deb2d5107483c06001cf3b0641
Marknjo <marknjo7@gmail.com>
2021-05-13 10:41:54 +0300

    Mapty: Rendering location pins on the map

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -55,6 +55,24 @@ if (navigator.geolocation) {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout!')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position!');
