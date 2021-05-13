a8aa0265ade80bf7df09a095dfb7beb862ae1f55
Marknjo <marknjo7@gmail.com>
2021-05-13 09:12:27 +0300

    Mapty: Intial project setup

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -13,9 +13,21 @@ const consoleSeparator = (title = './END', separatorLen = 20) => {
 
 //////////////////////////////////////////////
 //                                          //
-//          Starter Sections Separtor       //
+//             MAPTY APP PROJECT            //
 //                                          //
 //////////////////////////////////////////////
+
+// prettier-ignore
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+const form = document.querySelector('.form');
+const containerWorkouts = document.querySelector('.workouts');
+
+const inputType = document.querySelector('.form__input--type');
+const inputDistance = document.querySelector('.form__input--distance');
+const inputDuration = document.querySelector('.form__input--duration');
+const inputCadence = document.querySelector('.form__input--cadence');
+const inputElevation = document.querySelector('.form__input--elevation');
 
 //Separator for console logs
 /////////////////////////////////////////////////
