ee3eb6c7b5e846f31e1ea8ac5fb2d42185155a8e
Marknjo <marknjo7@gmail.com>
2021-05-14 10:36:34 +0300

    Async: XMHLHttpRequest - old school way of asyc calls

--- src/scripts/app/app.js
+++ src/scripts/app/app.js
@@ -22,6 +22,60 @@ const consoleSeparator = (title = './END', separatorLen = 20) => {
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
+//////////////////////////////////////////////
+//                                          //
+//          Asychronous JavaScript          //
+//               XMLHttpRequest             //
+//                                          //
+//////////////////////////////////////////////
+//
+
+//Old school way
+const getCountryData = country => {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+  request.send();
+
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+
+    const langs = data.languages.map(lang => lang.name);
+
+    let html = '';
+    html += `
+   <article class="country">
+      <img class="country__img" src="${data.flag}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>`;
+    langs.forEach((lang, i) => {
+      if (langs.length - 1 === i) {
+        html += ` ${langs.length === 1 ? '' : '&'} ${lang}`;
+      } else {
+        html += `${lang}${langs.length > 2 ? ',' : ''} `;
+      }
+    });
+
+    html += `</p>`;
+
+    html += `<p class="country__row"><span>💰</span>${data.currencies[0].code}</p>
+          </div>
+        </article>
+  `;
+
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+
+getCountryData('kenya');
+getCountryData('portugal');
+getCountryData('usa');
+
 //Separator for console logs
 /////////////////////////////////////////////////
 consoleSeparator(undefined, 40);
