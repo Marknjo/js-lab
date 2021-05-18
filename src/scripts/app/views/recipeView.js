7147ed2063e78482d8d01e4d07d87ff808ac1e18
Marknjo <marknjo7@gmail.com>
2021-05-18 15:00:16 +0300

    Forkify: adding fractional support for the ingredients counts

--- src/scripts/app/views/recipeView.js
+++ src/scripts/app/views/recipeView.js
@@ -1,10 +1,11 @@
 import consoleSeparator from '../app';
 //icons
 import svgIcon from '../../../assets/images/icons.svg';
-console.log(svgIcon);
-//
-/* import 'core-js/stable';
-import 'regenerator-runtime/runtime'; */
+
+//fractional
+import { Fraction } from 'fractional';
+
+console.log(Fraction);
 
 class RecipeView {
   //parent element
@@ -87,20 +88,7 @@ class RecipeView {
             <h2 class="heading--2">Recipe ingredients</h2>
             <ul class="recipe__ingredient-list">
                 ${this.#data.ingredients
-                  .map(
-                    ing => `
-                    <li class="recipe__ingredient">
-                        <svg class="recipe__icon">
-                        <use href="${svgIcon}#icon-check"></use>
-                        </svg>
-                        <div class="recipe__quantity">${ing.quantity}</div>
-                        <div class="recipe__description">
-                        <span class="recipe__unit">${ing.unit}</span>
-                        ${ing.description}
-                        </div>
-                    </li>
-                    `
-                  )
+                  .map(ing => this.#generateMarkupIngredients(ing))
                   .join('')}
             </ul>
         </div>
@@ -125,6 +113,23 @@ class RecipeView {
             </svg>
             </a>
         </div>
+    `;
+  }
+
+  #generateMarkupIngredients(ing) {
+    return `
+        <li class="recipe__ingredient">
+            <svg class="recipe__icon">
+            <use href="${svgIcon}#icon-check"></use>
+            </svg>
+            <div class="recipe__quantity">${
+              ing.quantity ? new Fraction(ing.quantity).toString() : ''
+            }</div>
+            <div class="recipe__description">
+            <span class="recipe__unit">${ing.unit}</span>
+            ${ing.description}
+            </div>
+        </li>
     `;
   }
 
