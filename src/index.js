02bccc5e2a3c87c5898f062f96450928b7381ede
Marknjo <marknjo7@gmail.com>
2021-06-12 17:34:30 +0300

    Marking the end of lessons on Firebase Auth

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 'use strict';
 /* Base Scripts import */
-// import './App/scripts/app';
-// import './App/styles/main.scss';
-import './firebase-auth/App/scripts/app';
-import './firebase-auth/App/scripts/configs/firebase';
+import './App/scripts/app';
+import './App/styles/main.scss';
 
 //enable hot reload
 if (module.hot) {
