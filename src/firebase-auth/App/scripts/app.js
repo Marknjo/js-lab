333bc06011fcf243596940cc3a0d97091acb04aa
Marknjo <marknjo7@gmail.com>
2021-06-12 10:01:36 +0300

    Firebase Auth: Initialize the project

--- src/firebase-auth/App/scripts/app.js
+++ src/firebase-auth/App/scripts/app.js
@@ -1,3 +1,12 @@
-import "./utils/utils";
+import { consoleSeparator } from './helpers/general';
 
-console.log("App");
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function () {
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+});
+
+consoleSeparator();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 'use strict';
 /* Base Scripts import */
-import './App/scripts/app';
-import './App/styles/main.scss';
+// import './App/scripts/app';
+// import './App/styles/main.scss';
+import './firebase-auth/App/scripts/app';
+import './firebase-auth/App/styles/main.scss';
 
 //enable hot reload
 if (module.hot) {
